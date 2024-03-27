c1634dc704f0a80af120ebfa79eb62fef2c54d92
gramizor <granin17@yandex.ru>
2024-03-27 18:42:29 +0300

    new input

--- client/src/Components/SendForm/SendForm.tsx
+++ client/src/Components/SendForm/SendForm.tsx
@@ -1,13 +1,18 @@
-import { Input } from '@mui/material'
+import { TextField } from '@mui/material'
 import React from 'react'
+import s from './SendForm.module.scss'
 
 type Props = {}
 
 const SendForm = (props: Props) => {
     return (
-        <div>
+        <div className={s.inputForm}>
             <form action="/send" method="post">
-                <Input sx={{ width:"100%" }}></Input>
+                <TextField
+                    sx={{ width: '100%' }}
+                    label="Введите сообщение"
+                    variant="outlined"
+                />
             </form>
         </div>
     )
