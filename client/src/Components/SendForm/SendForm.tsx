91241a10b319b62f8053b5115ad8a9787d3083f5
gramizor <granin17@yandex.ru>
2024-03-28 23:49:30 +0300

    final inpt

--- client/src/Components/SendForm/SendForm.tsx
+++ client/src/Components/SendForm/SendForm.tsx
@@ -1,18 +1,48 @@
-import { TextField } from '@mui/material'
-import React from 'react'
+import { Button, TextField, styled } from '@mui/material'
 import s from './SendForm.module.scss'
+import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
 
 type Props = {}
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 
 const SendForm = (props: Props) => {
     return (
         <div className={s.inputForm}>
-            <form action="/send" method="post">
-                <TextField
-                    sx={{ width: '100%' }}
-                    label="Введите сообщение"
-                    variant="outlined"
-                />
+            <form className={s.form}>
+                    <Button
+                        // component="label"
+                        role={undefined}
+                        aria-label="delete"
+                        // tabIndex={-1}
+                        color="inherit"
+                    >
+                        <AttachFileOutlinedIcon
+                            sx={{ fontSize: 40 }}
+                        />
+                        <VisuallyHiddenInput type="file" />
+                    </Button>
+                    <TextField
+                        sx={{ width: '100%', }}
+                        label="Введите сообщение"
+                        variant="outlined"
+                    />
+                <Button
+                    variant="contained"
+                    sx={{ padding: '0 25px', marginLeft: '30px' }}
+                >
+                    Отправить
+                </Button>
             </form>
         </div>
     )
